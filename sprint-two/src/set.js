07b7019d7fabb27e6e5284c5f108b78abc05a225
Anthony Cella <anthony.cella242@gmail.com>
2022-06-03 15:33:05 -0400

    Completed Set, still need additional test

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,41 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var itemFoundInStorage = false;
+
+  for (var currentIndex = 0; currentIndex < this._storage.length; currentIndex++) {
+    var currentItem = this._storage[currentIndex];
+    if (currentItem === item) {
+      itemFoundInStorage = true;
+      break;
+    }
+  }
+
+  return itemFoundInStorage;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    for (var currentIndex = 0; currentIndex < this._storage.length; currentIndex++) {
+      var currentItem = this._storage[currentIndex];
+
+      if (currentItem === item) {
+        this._storage = this._storage.slice(0, currentIndex).concat(this._storage.slice(currentIndex + 1));
+      }
+    }
+  }
 };
 
 /*
