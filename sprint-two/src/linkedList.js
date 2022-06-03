405f0a87bcb06a3ec8a2d816cfbe57f36e2ab036
Anthony Cella <anthony.cella242@gmail.com>
2022-06-03 11:42:48 -0400

    Completed Linked List, still need test for it

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,45 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // I: A value, any value whatsoever
+  // O: None - Side effect alters the value of the LinkedList
+  // C: Time complexity needs to be O(1)
+  // E: Linked List is empty - assign head with value
+
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      return undefined;
+    } else {
+      var previousHeadValue = list.head.value;
+      list.head = list.head.next;
+    }
+
+    return previousHeadValue;
   };
 
   list.contains = function(target) {
+    var containsTarget = false;
+
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        containsTarget = true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return containsTarget;
   };
 
   return list;
@@ -26,4 +58,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+   Insertion: O(1)
+
  */
