4f98613141b66d879c8e52c2ce8fe86aa56f9767
Anthony Cella <anthony.cella242@gmail.com>
2022-06-04 11:53:03 -0400

    Completed HashTable basics, still need test

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,44 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketArray = this._storage.get(index) === undefined ? [] : this._storage.get(index);
+  bucketArray.push([k, v]);
+  this._storage.set(index, bucketArray);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucketArray = this._storage.get(index);
+
+  var resultValue;
+  for (let i = 0; i < bucketArray.length; i++) {
+    if (bucketArray[i][0] === k) {
+      resultValue = bucketArray[i][1];
+    }
+  }
+
+  return resultValue;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketArray = this._storage.get(index);
+
+  var resultValue;
+  for (let i = 0; i < bucketArray.length; i++) {
+    if (bucketArray[i][0] === k) {
+      bucketArray.splice(i);
+    }
+  }
+
+  this._storage.set(index, bucketArray);
 };
 
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions
  */
 
 
